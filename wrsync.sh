6a2b2aa56b208a9dce9943e095694ca5ffb9ae4f
Dawid Grzegorz Węckowski <dawid.weckowski@kie.ue.poznan.pl>
2012-03-11 22:00:42 +0100

    dalej

--- wrsync.sh
+++ wrsync.sh
@@ -2,17 +2,6 @@
 
 # TODO: folder end slash
 
-#get options
-# while getopts d:D: name
-# do
-#     case $name in
-#         d)    CONFDIR="$OPTARG";;
-#         D)    MULTICONFDIR="$OPTARG";;
-#         ?)    printf "Usage: %s: [-d dir | -D dir] \n" $0
-#             exit 2;;
-#     esac
-# done
-
 function set-defaults {
 #DEFAULTS
 #    DEFAULTCONFDIR="~/.wrsync"
@@ -30,52 +19,28 @@ function set-defaults {
 }
 
 
-# function reset {
-#     echo reset
-#     CONFDIR=
-#     LOCALPATH=
-#     REMOTEHOST=
-#     REMOTEPATH=
-#     REMOTECONF=
-#     NEWSYNC=
-# }
-
 # init two-way sync, no delete, 
 function init-sync {
     echo init-sync
     rsync -azus --cvs-exclude "${REMOTEHOST}:${REMOTEPATH}" "${LOCALPATH}" 2>>"${RSYNCLOG}"
     if [ $? -gt 0 ]
     then
+        echo "Error while syncing from ${REMOTEHOST}:${REMOTEPATH}"  2>>"${RSYNCLOG}"
         exit 1
     fi
     rsync -azus --cvs-exclude "${LOCALPATH}" "${REMOTEHOST}:${REMOTEPATH}" 2>>"${RSYNCLOG}"
     if [ $? -gt 0 ]
     then
+        echo "Error while syncing to ${REMOTEHOST}:${REMOTEPATH}"  2>>"${RSYNCLOG}"
         exit 1
     fi
 }
 
 function sync-lastsync {
-    echo sync-lastsync
+    echo sync-lastsync >&2
     #TODO: exit on rsync failure
     rsync -azs --itemize-changes "${LASTSYNCFILE}" "${REMOTEHOST}:${REMOTECONF}${LASTSYNCFILENAME}" | wc -l
 }
-
-# # sync for every subdir of $MULTICONFDIR
-# function sync-all {
-#     echo sync-all
-#     if [ ! -d "${MULTICONFDIR}" ]
-#     then
-#         echo "No configuration directory: ${MULTICONFDIR}"
-#         exit 1
-#     fi
-#     for confdir in `find $MULTICONFDIR -type d`;
-#     do
-#         CONFDIR=$confdir
-#         sync
-#         reset
-#     done
-# }
 
 function sync {
     echo sync
@@ -83,7 +48,8 @@ function sync {
     if [ -f "${LASTSYNCFILE}" ]
     then
         NEWSYNC=`sync-lastsync`
-        if [ "${NEWSYNC}" == "0" ]
+        echo $NEWSYNC
+        if [ "${NEWSYNC}" = "0" ]
         then
             sync-to
         else
@@ -102,22 +68,33 @@ function sync {
 function prepare-filter {
     echo prepare-filter
     # find files in $LOCALPATH not modified after $LASTSYNCFILE
-    ssh "$REMOTEHOST" 'find '"${REMOTEPATH}"' -not -newer '"${REMOTECONF}${LASTSYNCFILENAME}"\
+    ssh "$REMOTEHOST" 'find '"${REMOTEPATH}"' -not -newermm '"${REMOTECONF}${LASTSYNCFILENAME}"\
+        '-or -not -newercc '"${REMOTECONF}${LASTSYNCFILENAME}"\
         ' -not -type d' | sort | sed 's|'"$REMOTEPATH"'|- |' > "${FILTERFILE}"
-    find "$LOCALPATH" -not -newer "${LASTSYNCFILE}" \
+    find "$LOCALPATH" -not -newermm "${LASTSYNCFILE}" \
+        -or -not -newercc "${LASTSYNCFILE}" \
         -not -type d | sort | sed 's|'"$LOCALPATH"'|R |' >> "${FILTERFILE}"
     echo "P *" >> "${FILTERFILE}"
 }
 
 function sync-from {
     echo sync-from
-    rsync -azus --delete --cvs-exclude --filter=". ${CONFDIR}/filter" \
-        "${REMOTEHOST}:${REMOTEPATH}" "${LOCALPATH}" 2>>"$RSYNCLOG"
+    LISTFROM=`rsync -azus --itemize-changes --delete --cvs-exclude --filter=". ${CONFDIR}/filter" \
+        "${REMOTEHOST}:${REMOTEPATH}" "${LOCALPATH}" 2>>"$RSYNCLOG"`
+    if [ -n "${LISTFROM}" ];
+    then
+        notify-send "${LISTFROM}"
+    fi
 }
 
 function sync-to {
     echo sync-to
-    rsync -azus --delete --cvs-exclude "${LOCALPATH}" "${REMOTEHOST}:${REMOTEPATH}" 2>>"$RSYNCLOG"
+    LISTTO=`rsync -azus --itemize-changes --delete --cvs-exclude "${LOCALPATH}" "${REMOTEHOST}:${REMOTEPATH}" \
+        2>>"$RSYNCLOG"`
+    if [ -n "${LISTTO}" ];
+    then
+        notify-send "${LISTTO}"
+    fi
 }
 
 # program main
